d43614190e880c710cd024a4b935706e94f87840
Patrick Forringer <patrick@forringer.com>
2015-07-27 22:19:19 -0500

    debug toolbar for local install

--- config/local.py
+++ config/local.py
@@ -22,3 +22,12 @@ class Local(Common):
     SECRET_KEY = os.environ.get(
         "SECRET_KEY", get_random_string(50, (
             "abcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*(-_=+)")))
+
+    INSTALLED_APPS = Common.INSTALLED_APPS
+
+    INSTALLED_APPS += ('debug_toolbar',)
+
+    MIDDLEWARE_CLASSES = Common.MIDDLEWARE_CLASSES
+    MIDDLEWARE_CLASSES += (
+        'debug_toolbar.middleware.DebugToolbarMiddleware',
+    )
