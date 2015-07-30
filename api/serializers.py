6c482091943dbc36cf642a794bcb4dfe84070c9c
Patrick Forringer <patrick@forringer.com>
2015-07-30 12:49:11 -0500

    read only meta and location for Location model on the API

--- api/serializers.py
+++ api/serializers.py
@@ -10,10 +10,11 @@ class LocationSerializer(GeoFeatureModelSerializer):
     resource_uri = serializers.HyperlinkedIdentityField(
         view_name='api:location-detail', read_only=True)
 
-    meta = StraightJSONField()
+    meta = StraightJSONField(read_only=True)
 
     class Meta:
         model = Location
         geo_field = 'location'
         id_field = False
         fields = ('id', 'name', 'resource_uri', 'address', 'location', 'meta')
+        read_only_fields = ('location',)
