545b84233261cd7939c70466bf0a22c4e5259abd
Patrick Forringer <patrick@forringer.com>
2015-08-01 11:04:19 -0500

    adding location serializer meta test

--- api/tests/test_serializers.py
+++ api/tests/test_serializers.py
@@ -2,6 +2,7 @@ from django.test import TestCase
 from rest_framework_gis.serializers import GeoFeatureModelSerializer
 
 from api.serializers import LocationSerializer
+from events.models import Location
 
 
 class TestLocationSerializer(TestCase):
@@ -10,4 +11,10 @@ class TestLocationSerializer(TestCase):
         self.assertIsInstance(LocationSerializer(), GeoFeatureModelSerializer)
 
     def test_meta_class(self):
-        pass
+        meta = LocationSerializer.Meta
+
+        self.assertEqual(meta.model, Location)
+        self.assertEqual(meta.geo_field, 'location')
+        self.assertEqual(meta.id_field, False)
+        self.assertEqual(meta.fields, ('id', 'name', 'resource_uri', 'address', 'location', 'meta'))
+        self.assertEqual(meta.read_only_fields, ('location',))
