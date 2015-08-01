c8459fa45ddf9b5c5a79dba059bad5ca47e5e84d
Patrick Forringer <patrick@forringer.com>
2015-08-01 10:34:28 -0500

    whitespace

--- usergroups/wsgi.py
+++ usergroups/wsgi.py
@@ -15,6 +15,5 @@ from whitenoise.django import DjangoWhiteNoise
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config")
 os.environ.setdefault("DJANGO_CONFIGURATION", "Local")
 
-
 application = get_wsgi_application()
 application = DjangoWhiteNoise(application)
