dd7c717f937675eeb414f4e899cf869d4b8323ce
Patrick Forringer <patrick@forringer.com>
2015-08-08 14:27:09 -0500

    Adding a base Event model mixin

---
+++ events/models/mixins.py
@@ -0,0 +1,39 @@
+import arrow
+from django.core.exceptions import ValidationError
+from django.db import models
+from django.utils.translation import ugettext_lazy as _
+from django_extensions.db.models import TimeStampedModel, TitleSlugDescriptionModel
+
+
+class EventModelMixin(TimeStampedModel, TitleSlugDescriptionModel):
+    """
+    Abstract base class to prevent code duplication.
+
+    `title` `description`, and `slug` fields inherited from djang-extensions TitleSlugDescriptionModel.
+
+    :start: The start date of the event.
+    :end: The end date of the event.
+    :title: The title of the event.
+    :slug: The url slug of the event.
+    :description: The description of the event.
+    """
+    start = models.DateTimeField(
+        verbose_name=_('Start time'),
+    )
+
+    end = models.DateTimeField(
+        verbose_name=_('End time'),
+    )
+
+    def __str__(self):
+        return "{title} ({start})".format(title=self.title, start=arrow.get(self.start).format('MMM D, YYYY'))
+
+    def clean(self):
+        super().clean()
+
+        if (self.end and self.start) and self.end < self.start:
+            #REVIEW: would be nice if this was a part of the field validators
+            raise ValidationError("Start time must be earlier than end time.")
+
+    class Meta:
+        abstract = True
---
+++ events/tests/test_model_mixins.py
@@ -0,0 +1,43 @@
+# -*- coding: utf-8 -*-
+"""Tests for events models."""
+from datetime import datetime
+
+from django.core.exceptions import ValidationError
+from django.test import TestCase
+from django_extensions.db.models import TimeStampedModel, TitleSlugDescriptionModel
+from model_mommy import mommy
+import fudge
+
+
+from events.models.mixins import EventModelMixin
+
+
+class TestEventModelMixin(TestCase):
+    def setUp(self):
+        self.model = mommy.prepare(EventModelMixin, title='Event Title', start=datetime(2015, 8, 1))
+
+    def test_attr(self):
+        self.assertEqual(str(self.model), 'Event Title (Aug 1, 2015)')
+
+    def test_Meta(self):
+        self.assertEqual(EventModelMixin._meta.abstract, True)
+
+    def test_mro(self):
+        self.assertIsInstance(self.model, TimeStampedModel)
+        self.assertIsInstance(self.model, TitleSlugDescriptionModel)
+
+    def test_clean_end_date_sooner_than_start(self):
+
+        self.model.start = datetime(2015, 1, 10)
+        self.model.end = datetime(2015, 1, 1)
+
+        with self.assertRaises(ValidationError) as cm:
+            self.model.clean()
+
+        self.assertEqual(cm.exception.message, "Start time must be earlier than end time.")
+
+    def test_clean_end_date_later_than_start(self):
+        self.model.start = datetime(2015, 1, 10)
+        self.model.end = datetime(2015, 1, 13)
+
+        self.model.clean()
