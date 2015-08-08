ad15143f2c8552f1d8300173eb419c2c6667efb1
Patrick Forringer <patrick@forringer.com>
2015-08-08 14:29:55 -0500

    moving Location model to event meta data models module

--- events/models/meta.py
+++ events/models/meta.py
@@ -4,6 +4,7 @@ from hashlib import md5
 from django.contrib.gis.db import models
 from django.contrib.gis.geos import Point
 from django.utils.translation import ugettext_lazy as _
+from django_extensions.db.models import TimeStampedModel
 from django_pgjson.fields import JsonField
 
 from utils.geo import geocode
@@ -12,11 +13,35 @@ from utils.geo import geocode
 class Location(models.Model):
     """Geo and meta representation of a location"""
 
-    name = models.CharField(_('Location Name'), max_length=255)
-    address = models.CharField(_('Address'), max_length=255, blank=True, null=True, unique=True)
-    location = models.PointField(_('Location'), blank=True, null=True)
-    location_hash = models.CharField(max_length=32, null=True)
-    meta = JsonField(_('metadata'), blank=True, null=True)
+    name = models.CharField(
+        _('Location Name'),
+        max_length=255
+    )
+
+    address = models.CharField(
+        _('Address'),
+        max_length=255,
+        blank=True,
+        null=True,
+        unique=True
+    )
+
+    location = models.PointField(
+        _('Location'),
+        blank=True,
+        null=True
+    )
+
+    location_hash = models.CharField(
+        max_length=32,
+        null=True
+    )
+
+    meta = JsonField(
+        _('metadata'),
+        blank=True,
+        null=True
+    )
 
     objects = models.GeoManager()
 
@@ -25,15 +50,15 @@ class Location(models.Model):
 
     def save(self, **kwargs):
         location_hash = md5(
-            '{}@{}'.format(self.name, self.address).encode('utf-8'))
+            '{}@{}'.format(self.name, self.address).encode('utf-8')).hexdigest()
         if self.location_hash != location_hash and self.address:
             location_geocode = geocode(self.address)
             if location_geocode:
                 self.meta = location_geocode.raw
-                pos = [float(coord) for coord in [self.meta.get('lon'), self.meta.get('lat')]]
+                pos = [float(self.meta.get(coord)) for coord in ['lon', 'lat']]
                 self.location = Point(*pos)
                 self.location_hash = location_hash
             else:
                 self.meta = {"geocoded": False}
                 self.location = None
-        super(Location, self).save(**kwargs)
+        super().save(**kwargs)
--- events/tests/test_meta_models.py
+++ events/tests/test_meta_models.py
@@ -5,7 +5,7 @@ from django.test import TestCase
 from model_mommy import mommy
 import fudge
 
-from events.models import Location
+from events.models.meta import Location
 
 
 class LocationModelTest(TestCase):
@@ -18,13 +18,14 @@ class LocationModelTest(TestCase):
     def test_str(self):
         self.assertEqual(str(self.location), 'Location Name')
 
-    @fudge.patch('events.models.md5')
-    @fudge.patch('events.models.geocode')
+    @fudge.patch('events.models.meta.md5')
+    @fudge.patch('events.models.meta.geocode')
     def test_save_with_matched_location_hash(self, md5, geocode):
         self.location.location = Point([123, 123])
         self.location.location_hash = 'this'
 
-        md5.expects_call().with_args(b'Location Name@an address').returns('this')
+        (md5.expects_call().with_args(b'Location Name@an address').returns_fake()
+            .expects('hexdigest').returns('this'))
 
         self.location.save()
 
@@ -35,13 +36,14 @@ class LocationModelTest(TestCase):
         # hash is unchanged
         self.assertEqual(self.location.location_hash, 'this')
 
-    @fudge.patch('events.models.md5')
-    @fudge.patch('events.models.geocode')
+    @fudge.patch('events.models.meta.md5')
+    @fudge.patch('events.models.meta.geocode')
     def test_save_with_missed_location_hash_and_address_present(self, md5, geocode):
         self.location.location = Point([123, 123])
         self.location.location_hash = 'not this'
 
-        md5.expects_call().with_args(b'Location Name@an address').returns('this')
+        (md5.expects_call().with_args(b'Location Name@an address').returns_fake()
+            .expects('hexdigest').returns('this'))
         mock_location_geocode = fudge.Fake('location_geocode').has_attr(
             raw={'raw': 'data', 'lat': '123.4', 'lon': '-12.3'})
         geocode.expects_call().with_args('an address').returns(mock_location_geocode)
@@ -56,13 +58,14 @@ class LocationModelTest(TestCase):
         # hash is updated
         self.assertEqual(self.location.location_hash, 'this')
 
-    @fudge.patch('events.models.md5')
-    @fudge.patch('events.models.geocode')
+    @fudge.patch('events.models.meta.md5')
+    @fudge.patch('events.models.meta.geocode')
     def test_save_with_missed_location_hash_and_address_present_geocode_fail(self, md5, geocode):
         self.location.location = Point([123, 123])
         self.location.location_hash = 'not this'
 
-        md5.expects_call().with_args(b'Location Name@an address').returns('this')
+        (md5.expects_call().with_args(b'Location Name@an address').returns_fake()
+            .expects('hexdigest').returns('this'))
         geocode.expects_call().with_args('an address').returns(None)
 
         self.location.save()
@@ -75,14 +78,15 @@ class LocationModelTest(TestCase):
         # hash is not updated
         self.assertEqual(self.location.location_hash, 'not this')
 
-    @fudge.patch('events.models.md5')
-    @fudge.patch('events.models.geocode')
+    @fudge.patch('events.models.meta.md5')
+    @fudge.patch('events.models.meta.geocode')
     def test_save_with_missed_location_hash_no_address(self, md5, geocode):
         self.location.location = Point([123, 123])
         self.location.location_hash = 'not this'
         self.location.address = None
 
-        md5.expects_call().with_args(b'Location Name@None').returns('this')
+        (md5.expects_call().with_args(b'Location Name@None').returns_fake()
+            .expects('hexdigest').returns('this'))
 
         self.location.save()
 
