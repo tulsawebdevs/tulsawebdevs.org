d5f4f99e7874ec962dd3a51ffd992dc42e05845b
Patrick Forringer <patrick@forringer.com>
2015-09-01 21:03:08 -0500

    A list of upcoming events on the homepage

--- twd/views.py
+++ twd/views.py
@@ -1,6 +1,10 @@
+import arrow
+from django.utils.timezone import datetime, now
 from django.views.generic import TemplateView
 
 from events import views as base_views
+
+from events.models import Event
 
 
 class UpcomingOccurrencesView(base_views.UpcomingOccurrencesViewBase):
@@ -13,6 +17,19 @@ class EventsListView(base_views.EventsListViewBase):
 
 class HomePageView(TemplateView):
     template_name = 'twd/home.jinja'
+
+    def get_context_data(self):
+        context = super().get_context_data()
+        context.update({
+            'get_upcoming_events': self.get_upcoming_events
+        })
+        return context
+
+    def get_upcoming_events(self):
+        """Grab all occurrences from a week ago to a month in the future"""
+        start_from = arrow.utcnow().replace(weeks=-1).naive
+        until = arrow.utcnow().replace(months=1).naive
+        return Event.objects.get_occurrences(start_from, until)
 
 
 # Event/Occurrence Views
